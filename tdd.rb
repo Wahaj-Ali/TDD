d45a7aed15f88d8d7ce49c651933182eb8ee2a3f
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-03-16 17:24:25 +0500

    Update tdd.rb
    In this commit
    - Add reverse method for the Solver class.

--- tdd.rb
+++ tdd.rb
@@ -4,11 +4,14 @@
 class Solver
   def factorial(number)
     return 1 if number.zero?
-
     start = 1
     (1..number).each do |n|
       start *= n
     end
     start
+    end
+  
+  def reverse(string)
+    string.chars.reverse.join
   end
 end
