57597c4f3b4d792836929dd9b993ee4c7b21ff48
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-03-16 17:17:16 +0500

    Create tdd.rb
    In this commit
    - Add the factorial method for the Solver class.

---
+++ tdd.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Solver class with factorial, revere and fizzbuzz methods.
+class Solver
+  def factorial(number)
+    return 1 if number.zero?
+
+    start = 1
+    (1..number).each do |n|
+      start *= n
+    end
+    start
+  end
+end
