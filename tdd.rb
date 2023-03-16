2386a6914d7ad166d8abc03025ca6bbdd7cd2c11
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-03-16 17:24:54 +0500

    Update tdd.rb
    Remove linter errors.

--- tdd.rb
+++ tdd.rb
@@ -4,13 +4,14 @@
 class Solver
   def factorial(number)
     return 1 if number.zero?
+
     start = 1
     (1..number).each do |n|
       start *= n
     end
     start
-    end
-  
+  end
+
   def reverse(string)
     string.chars.reverse.join
   end
